7e02d7d0cb90cb99cf73cbf17b966b69d600e722
VitorInc <vitordc.contato@gmail.com>
2021-05-25 03:09:04 -0300

    Created th application

--- src/Application.java
+++ src/Application.java
@@ -1,2 +1,8 @@
-package PACKAGE_NAME;public class Application {
+import entities.Carro;
+
+public class Application {
+    public static void main(String... args){
+        Carro carro = new Carro(4, 5,1,"Gasolina", "Azul",2021, 1234564789);
+        carro.imprimeValores();
+    }
 }
